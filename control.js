0f2db631784a6bca9b1da7cd094fc67fa9954892
IndayaraSilva <indayarasilva@gmail.com>
2022-05-04 21:06:54 -0300

    Atividade completa

---
+++ control.js
@@ -0,0 +1,25 @@
+debugger;
+
+var qtdFrases = parseInt(prompt("Quantas frases você quer digitar?"));
+var frases = [];
+var frase = "";
+
+// Identificar a quantidade de frases para perguntar [FUNCIONANDO]
+while (frases.length < qtdFrases) {
+    var frase = prompt("Digite a sua frase")
+    frases.push(frase);
+}
+
+// Verificar caracteres
+var frasesComCaracter = [];
+var quaisCaracteres = prompt("Quais caracteres você quer identificar nas frases?");
+
+// FUNCIONOU Basicamente, to comparando uma frase/palavra inteira com um caracter. Não tem como isso dar certo.
+for (var indice = 0; indice < frases.length; indice ++) {
+    if (frases[indice].toLowerCase().includes(quaisCaracteres.toLowerCase())) { 
+        frasesComCaracter.push(frases[indice])
+    }
+}
+
+alert("Frases ditas: " + frases +
+    "\nFrases com o caractere escolhido: " + frasesComCaracter); 
